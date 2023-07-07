cf48ab5b11f2c4e2e5ecb91623c36af12d54c7dd
June-Senpai <keshav01juneja@gmail.com>
2023-07-08 05:01:58 +0530

    project done jst abit of editing left

--- src/components/AboutComponents/Projects.jsx
+++ src/components/AboutComponents/Projects.jsx
@@ -1,29 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Projects = () => {
+const ProjectsPart = () => {
   const projects = [
-    { name: "Project 1", description: "Description of Project 1" },
-    { name: "Project 2", description: "Description of Project 2" },
-    { name: "Project 3", description: "Description of Project 3" },
+    {
+      name: "File Uploader",
+      description:
+        "A web application built with React and Node.js that allows users to securely upload and manage files using Google authentication. After signing in, users can upload any type of file to a local directory on the web server. The application also provides a list of all uploaded files, with the ability to download individual files.",
+      imageUrl: "/projects/juneDrive.png",
+      githubUrl: "https://github.com/June-Senpai/file-uploader",
+      demoUrl: "https://june-drive.netlify.app",
+    },
+    {
+      name: "Notes",
+      description:
+        "A note-taking web application built with the MERN stack that features an autosave capability. Users can create and manage notes with rich text processing capabilities, thanks to the integration of the Quill library. Notes are automatically saved to a MongoDB database, preserving changes even if the user navigates away from the page. Each note has a unique URL that can be shared with others.",
+      imageUrl: "/projects/notes.png",
+      githubUrl: "https://github.com/June-Senpai/autosave-blog",
+      demoUrl: "https://notes-june.netlify.app",
+    },
+    {
+      name: "Manager",
+      description:
+        "A web application built with Next.js and MongoDB that allows users to manage vendor information. Features include login with Google, creating and editing vendor details, and displaying a paginated list of vendors. Users can easily update vendor information or delete vendors after confirmation.",
+      imageUrl: "/projects/vendorList.png",
+      githubUrl: "https://github.com/June-Senpai/Vendor-Manager",
+      demoUrl: "https://vendor-manager-three.vercel.app",
+    },
   ];
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === projects.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevClick = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
 
   return (
-    <div style={{ overflowX: "scroll", whiteSpace: "nowrap" }}>
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          style={{
-            display: "inline-block",
-            width: "300px",
-            margin: "10px",
-          }}
-        >
-          <h2>{project.name}</h2>
-          <p>{project.description}</p>
+    <div className="flex flex-col items-center ">
+      <div className="text-8xl font-bold text-pink-300 mx-auto mb-11">
+        <h1>
+          <span class="text-8xl font-extrabold bg-clip-text text-transparent bg-center bg-[url('https://images.unsplash.com/photo-1589307004173-3c95204d00ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80')]">
+            My
+          </span>
+          <span class="text-8xl font-extrabold bg-clip-text text-transparent bg-center bg-[url('https://images.unsplash.com/photo-1587738433410-8f34adcb0c5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-position-x-right">
+            Projects
+          </span>
+        </h1>
+      </div>
+
+      <div className="m-4 relative overflow-hidden">
+        <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+          <button
+            onClick={handlePrevClick}
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-pink-300 rounded-lg text-white mx-2 z-20"
+          >
+            <ion-icon name="caret-back-outline"></ion-icon>
+          </button>
+          <button
+            onClick={handleNextClick}
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-pink-300 rounded-lg text-white mx-2 z-20"
+          >
+            <ion-icon name="caret-forward-outline"></ion-icon>
+          </button>
+          <div className="">
+            <img
+              src={projects[currentProjectIndex].imageUrl}
+              alt={projects[currentProjectIndex].name}
+              className="w-full h-[500px] object-cover transition-transform duration-900 group-hover:scale-105 ease-in-out"
+            />
+          </div>
+          <div
+            className={`absolute inset-0 bg-gradient-to-b from-transparent via-transparent ${
+              currentProjectIndex === 2 ? "to-black" : "to-pink-50"
+            } group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70`}
+          ></div>
+          <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <h1 className="font-serif text-3xl font-bold text-pink-300">
+              {projects[currentProjectIndex].name}
+            </h1>
+            <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 mx-8">
+              {projects[currentProjectIndex].description}
+            </p>
+            <a
+              href={projects[currentProjectIndex].githubUrl}
+              target="_blank"
+              rel="noreferrer"
+              className=" flex items-center px-4 py-2 bg-neutral-900 shadow-black/60 rounded-full text-white mx-2 text-lg font-com capitalize"
+            >
+              <ion-icon name="logo-github" className="mr-2"></ion-icon>
+              <span className="ml-1">GitHub</span>
+            </a>
+            <div className="mt-2"></div>
+            <a
+              href={projects[currentProjectIndex].demoUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="px-4 py-2 bg-pink-500 shadow-black/60 rounded-full text-white mx-2 text-lg font-com capitalize"
+            >
+              Demo
+            </a>
+          </div>
         </div>
-      ))}
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={handlePrevClick}
+          className="px-4 py-2 bg-pink-300 rounded-lg text-white mx-2"
+        >
+          Prev
+        </button>
+        {projects.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              setCurrentProjectIndex(index);
+            }}
+            className={`w-4 h-4 rounded-full mx-2 cursor-pointer transition-all duration-300 ${
+              currentProjectIndex === index ? "bg-pink-300" : "bg-gray-300"
+            } hover:bg-pink-200 hover:scale-[1.3]`}
+          />
+        ))}
+
+        <button
+          onClick={handleNextClick}
+          className="px-4 py-2 bg-pink-300 rounded-lg text-white mx-2"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Projects;
+export default ProjectsPart;
