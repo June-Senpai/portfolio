5d2cbc913ad9a7a4cf6c09e0bce59aafd28f8d34
June-Senpai <keshav01juneja@gmail.com>
2023-07-11 14:13:02 +0530

    removed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex bg-pink-50 md:block">
-      <footer className="flex w-full h-24 border-t-2 mt-auto justify-center items-center">
+    <div className="flex bg-pink-50 md:block 2xl:absolute 2xl:bottom-0 2xl:w-[100%]">
+      <footer className="flex w-full h-24 border-t-2 mt-auto justify-center items-center ">
         <p className="text-gray-600">
           © 2023 Keshav Juneja . No rights reserved.
         </p>
